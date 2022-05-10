81a93cd033907bdf7ec6c4729e28bc550fe64583
ashikmahmud <ashikmamud187@gmail.com>
2022-05-10 23:47:20 +0600

    addded testimonial section

---
+++ src/Pages/Home/Appointment/Appointment.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import bgAppointment from "../../../Assets/images/appointment.png";
+import doctorImage from "../../../Assets/images/doctor-small.png";
+const Appointment = () => {
+  return (
+    <div
+      className="hero my-16 py-10"
+      style={{ background: `url(${bgAppointment})` }}
+    >
+      <div className="hero-content justify-around flex-col lg:flex-row">
+        <div className="lg:w-3/6 relative">
+          <img
+            src={doctorImage}
+            className="max-w-xl "
+            style={{ marginBottom: "-3.5rem;", marginTop: "-9rem" }}
+            alt=""
+          />
+        </div>
+        <div className="lg:w-3/6 text-base-100">
+          <h3 className="text-lg text-secondary">Appointment</h3>
+          <h1 className="text-3xl font-bold">Make an appointment Today</h1>
+          <p className="py-6">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsumis that it has a more-or-less normal
+            distribution of letters,as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page
+          </p>
+          <button className="btn text-white bg-gradient-to-r from-primary to-secondary border-0">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Appointment;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,14 +1,41 @@
 import React from "react";
+import treatment from "../../../Assets/images/treatment.png";
+import Appointment from "../Appointment/Appointment";
 import Hero from "../Hero/Hero";
 import Infos from "../Infos/Infos";
 import Services from "../Services/Services";
-
 const Home = () => {
   return (
     <section id="home">
       <Hero />
       <Infos />
       <Services />
+      {/* featured  */}
+      <div className="hero py-20">
+        <div className="hero-content justify-center gap-20 flex-col lg:flex-row">
+          <img
+            src={treatment}
+            className="max-w-sm rounded-lg shadow-2xl"
+            alt=""
+          />
+          <div className="w-2/4">
+            <h1 className="text-5xl font-bold">
+              Exceptional Dental Care, on Your Terms
+            </h1>
+            <p className="py-6">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsumis that it has a more-or-less normal
+              distribution of letters,as opposed to using 'Content here, content
+              here', making it look like readable English. Many desktop
+              publishing packages and web page
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      {/* end featured  */}
+      <Appointment />
     </section>
   );
 };
