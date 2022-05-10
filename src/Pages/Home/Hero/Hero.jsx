af3f3de7b8284d1321337a90ea6bbe5bb3b11020
ashikmahmud <ashikmamud187@gmail.com>
2022-05-10 21:50:35 +0600

    added hero section of this application

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,9 +1,32 @@
 import React from "react";
-
+import bgImage from "../../../Assets/images/bg.png";
+import heroImage from "../../../Assets/images/chair.png";
 const Hero = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Hero the super</h1>
+    <div
+      className="hero py-40 md:py-64 bg-cover md:bg-contain bg-no-repeat"
+      style={{
+        background: `url(${bgImage})`,
+      }}
+    >
+      <div className="hero-content flex-col lg:flex-row-reverse justify-between">
+        <img
+          src={heroImage}
+          className="md:max-w-md rounded-lg shadow-2xl"
+          alt="hero"
+        />
+        <div className="lg:w-3/6 mt-10">
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn bg-gradient-to-r from-primary to-secondary text-white">
+            Get Started
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -17,18 +17,18 @@ const Header = () => {
       <li>
         <Link to="/contact">Contact Us</Link>
       </li>
-      <li tabindex="0">
+      <li tabIndex="0">
         <Link to="/about">About</Link>
       </li>
     </>
   );
 
   return (
-    <header className="bg-slate-50 py-3">
+    <header className=" py-3 absolute w-full">
       <div className="navbar container mx-auto ">
         <div className="navbar-start">
           <div className="dropdown">
-            <label tabindex="0" className="btn btn-ghost lg:hidden">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -52,7 +52,7 @@ const Header = () => {
             </ul>
           </div>
           <Link to="/" className="btn btn-ghost normal-case text-xl">
-            Doctors Portal
+            Doctors <span className="text-primary"> Para</span>
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
